8c6fd830e719be61a7baaca69099b36dc748b2ca
Silvio Incalza <silvio@mincalza.me>
2023-12-02 20:23:56 +0100

    implementing day2

--- src/test/java/me/incalza/aoc2023/days/_2/ElfiCubeConundrumDecoderTest.java
+++ src/test/java/me/incalza/aoc2023/days/_2/ElfiCubeConundrumDecoderTest.java
@@ -22,6 +22,14 @@ class ElfiCubeConundrumDecoderTest {
         assertThat(decode).isEqualTo(8);
     }
 
+    @Test
+    void testPart1() throws Exception {
+        InputReader inputReader = InputReader.of(Path.of(getClass().getResource("input.txt").toURI()));
+        ElfiCubeConundrumDecoder decoder = new ElfiCubeConundrumDecoder(inputReader, new GameRecordLineParser(),new CubeConfig(12, 13, 14));
+        int decode = doDecode(decoder);
+        assertThat(decode).isEqualTo(2486);
+    }
+
 
     private int doDecode(ElfiCubeConundrumDecoder decoder) throws Exception {
         int decode = decoder.decode();
